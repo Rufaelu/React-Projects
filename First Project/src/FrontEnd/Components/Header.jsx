f75f0f09ab555c88514d565554d445e6ede0a623
Rufaelu <niamraf12@gmail.com>
2025-06-07 00:02:20 +0300

    .

---
+++ First Project/src/FrontEnd/Components/Header.jsx
@@ -0,0 +1,23 @@
+import './header.module.css'
+
+function Header(){
+   return (
+     <>
+       <header className="box-border ">
+         <nav className="flex space-x-4">
+           <span className='text-purple-400 text-xs'>
+             Technoit | IT Solutions & Business Services Multipurpose Responsive
+             Website Template
+           </span>
+           <div>dsfaf</div>
+           <div>dsfaf</div>
+           <div>dsfaf</div>
+         </nav>
+       </header>
+       <h1 class="text-xl font-bold underline">Hello world!</h1>
+     </>
+   );
+
+}
+
+export default Header;
